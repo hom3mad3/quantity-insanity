bdd856f393ff29fff29408d006d6f29d2dc2f381
hom3mad3 <micaelarava@gmail.com>
2017-05-11 22:41:45 +0200

    change spelling

--- graph/graph.rb
+++ graph/graph.rb
@@ -1,17 +1,17 @@
 require 'pry'
 
-# a node whi ch understands its neighbours
+# a node whi ch understands its neighbors
 class Graph
   attr_reader :name
-  attr_accessor :neighbours
+  attr_accessor :neighbors
 
   def initialize(name)
     @name = name
-    @neighbours = []
+    @neighbors = []
   end
 
-  def new_neighbour(node)
-    neighbours.push(node)
+  def new_neighbor(node)
+    neighbors.push(node)
     self
   end
 
@@ -19,13 +19,13 @@ class Graph
     return true if self == destination
     return false if visited_nodes.include?(self)
     visited_nodes.push(self)
-    neighbours.any? {|node| node.can_reach?(destination, visited_nodes) }
+    neighbors.any? {|node| node.can_reach?(destination, visited_nodes) }
   end
 end
 
 a, b, c = Graph.new('a'), b = Graph.new('b'), Graph.new('c')
 
-a.new_neighbour(b)
-b.new_neighbour(c)
+a.new_neighbor(b)
+b.new_neighbor(c)
 
 a.can_reach?(c, [])
--- graph/spec/graph_spec.rb
+++ graph/spec/graph_spec.rb
@@ -2,18 +2,18 @@ require_relative './spec_helper'
 require_relative '../graph.rb'
 
 describe Graph do
-  it 'checks for neighbours' do
+  it 'checks for neighbors' do
     a, b = Graph.new('a'), b = Graph.new('b')
 
-    expect(a.neighbours).to eq([])
-    expect(a.new_neighbour(b).neighbours).to eq([b])
+    expect(a.neighbors).to eq([])
+    expect(a.new_neighbor(b).neighbors).to eq([b])
   end
 
   it 'can a reach b' do
     a, b, c = Graph.new('a'), b = Graph.new('b'), Graph.new('c')
 
-    a.new_neighbour(b)
-    b.new_neighbour(c)
+    a.new_neighbor(b)
+    b.new_neighbor(c)
     #expect(a.can_reach?(b, [])).to be true
     expect(a.can_reach?(c, [])).to be true
   end
